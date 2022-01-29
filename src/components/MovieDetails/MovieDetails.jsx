0426c38caa7daf92f495a989193652539fba4d91
Sabrina Ciaciura <ciaci0028@gmail.com>
2022-01-29 11:47:22 -0600

    displaying description and added home button

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,13 +1,26 @@
 import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 function MovieDetails () {
+    const history = useHistory();
 
-    const selectedMovie = useSelector(store => store.selectedMovie);
-    console.log('details for:', selectedMovie);
+    const movie = useSelector(store => store.selectedMovie);
+    console.log('details for:', movie);
+
+    const routeHome = () => {
+        history.push('/')
+    }
 
     return (
         <>
             <h1>Movie Details</h1>
+            <h3>{movie.title}</h3>
+            <img
+                src={movie.poster}
+            />
+            <p>{movie.description}</p>
+
+            <button onClick={() => routeHome()}>Back to Home Page</button>
         </>
     )
 };
