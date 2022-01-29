9e10e3bbb3d1a44760849a004992907d804c4565
Sabrina Ciaciura <ciaci0028@gmail.com>
2022-01-29 15:58:56 -0600

    added inputs and collecting data from inputs

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import {HashRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList';
 import MovieDetails from '../MovieDetails/MovieDetails';
+import MovieForm from '../MovieForm/MovieForm';
 
 function App() {
   return (
